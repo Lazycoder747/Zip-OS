bd507f9ff55c9928d0c491c9427e90289168ca2e
Lazycoder747 <121690560+Lazycoder747@users.noreply.github.com>
2023-07-27 23:35:31 +0100

    Update kernel.asm

--- kernel.asm
+++ kernel.asm
@@ -9,12 +9,38 @@ _start:
     mov edx, msg_len    ; Length of the message in bytes
     int 0x80            ; Perform the system call
 
+    ; Call the 'kmain' function in C
+    call kmain
+
     ; Exit the program
     mov eax, 1          ; '1' is the system call number for sys_exit
     xor ebx, ebx        ; Exit status 0
     int 0x80            ; Perform the system call
 
+section .text
+    global shutdown
+
+shutdown:
+    ; Implement the shutdown function here.
+    ; This function should trigger the system shutdown sequence.
+    ; For simplicity, we'll just print a "Shutting down..." message.
+
+    ; Write "Shutting down..." to the screen
+    mov eax, 4          ; '4' is the system call number for sys_write
+    mov ebx, 1          ; '1' is the file descriptor for stdout
+    mov ecx, shutdown_msg ; Load the memory address of 'shutdown_msg' into ecx
+    mov edx, shutdown_msg_len ; Length of the message in bytes
+    int 0x80            ; Perform the system call
+
+    ; TODO: Add the actual shutdown sequence here
+
+    ret
+
 section .data
     msg db 'Hello, World!',0x0A   ; The message to be printed (with newline)
     msg_len equ $ - msg           ; Calculate the length of the message
-; Now we can print "Hello, World!" to the screen!
+
+section .data
+    shutdown_msg db 'Shutting down...',0x0A  ; The shutdown message (with newline)
+    shutdown_msg_len equ $ - shutdown_msg    ; Calculate the length of the message
+
